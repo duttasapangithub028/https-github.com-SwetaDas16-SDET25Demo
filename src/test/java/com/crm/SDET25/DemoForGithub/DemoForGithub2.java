10989baf507595098ffede7b4ba43540044bf3f8
sweta <sweta@Sweta>
2021-12-09 16:10:49 +0530

    Fourth

---
+++ src/test/java/com/crm/SDET25/DemoForGithub/DemoForGithub2.java
@@ -0,0 +1,13 @@
+package com.crm.SDET25.DemoForGithub;
+
+import org.testng.annotations.Test;
+
+public class DemoForGithub2 {
+@Test
+public void demo() {
+	System.out.println("For Commit Operation1 in Github");
+	System.out.println("For Commit Operation2 in Github");
+	System.out.println("For Commit Operation3 in Github");
+	System.out.println("For Commit Operation4 in Github");
+}
+}
