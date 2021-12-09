028b872c9eeb29faac811f61d309e7fbe9633977
sweta <sweta@Sweta>
2021-12-09 13:38:55 +0530

    Performing Commit Operation in Github

---
+++ src/test/java/com/crm/SDET25/DemoForGithub/DemoForGithub.java
@@ -0,0 +1,10 @@
+package com.crm.SDET25.DemoForGithub;
+
+import org.testng.annotations.Test;
+
+public class DemoForGithub {
+@Test
+public void demo() {
+	System.out.println("For Commit Operation in Github");
+}
+}
